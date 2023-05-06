4fa51bf248e48764b5e1f9dff472aca4fa17d4fc
esldz <esmanuryl1225@gmail.com>
2023-05-06 14:21:26 +0300

    document object model js dosyası eklendi

---
+++ js/document-object-model.js
@@ -0,0 +1,11 @@
+console.log(document.location.hostname)
+//çıktı: 127.0.0.1
+console.log(document.location.pathname)
+//çıktı: index.html dosyasının bulunduğu yer
+
+console.log(document.body)
+console.log(document.body)
+
+console.log(document.URL)
+console.log(document.baseURI)
+document.body.style.backgroundColor="aqua"
